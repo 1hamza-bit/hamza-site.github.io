c8806fba2b404ec32585d75adb8e3fa23e84e390
1hamza-bit <76727465+1hamza-bit@users.noreply.github.com>
2021-01-01 05:34:07 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+            <!-- links-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="images/jdm.png" type="image/gif" sizes="32x32">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- CSS only -->
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+   <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- font owesome-->
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<link rel="stylesheet" type="text/css" href="css/INDEX.css">
+
+
+<!-- inline css-->
+
+<style type="text/css">
+
+
+
+      #myVideo {
+    height: 600px;
+    width: 100%;
+
+    opacity: 5px;
+    margin-top: -200px;
+
+ 
+}
+
+
+
+     .myimage{
+
+
+           height: 100px;
+    width: 100%;
+
+    opacity: 1px;
+
+    margin-top: 100px;
+
+
+    margin-bottom: -100px;
+
+      filter: blur(2px);
+  -webkit-filter: blur(2px);
+ 
+    
+
+
+     }
+
+     .myimage:hover{
+
+      opacity:1;
+
+       background-color: red;
+
+     }
+
+
+ 
+
+
+
+.ul.a{
+     
+float: right;
+
+margin-top: -60px;
+
+margin-right: 40px;
+
+display: inline;
+
+
+
+}
+
+
+.ul.b {
+  list-style-type: square;
+}
+
+.li.a{
+	
+    display: inline;
+	padding-right: 10px;
+
+}
+
+h1{
+
+    color: black;
+
+
+
+
+}
+
+
+
+
+	</style>
+
+
+
+	<title> JDM CULTURE</title>
+	
+</head>
+
+
+
+ <!-- body starting -->
+<body >
+
+	<div class="navbar1">
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <a class="navbar-brand" href="#"><img src="images/logo.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa fa-bars"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</div>
+
+
+
+
+</body>
+</html>
